a85309e60d2e8defe9b946ea95a434c3c0a38ef9
Jagadeeswara Yaramala <yjagspi@gmail.com>
2019-10-22 11:30:37 +0530

    Modal Pop-up added on click of a button

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { MainComponent } from './components/main/main.component';
+import { ScatterPlotComponent } from './components/scatter-plot/scatter-plot.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    MainComponent
+    MainComponent,
+    ScatterPlotComponent
   ],
   imports: [
     BrowserModule,
