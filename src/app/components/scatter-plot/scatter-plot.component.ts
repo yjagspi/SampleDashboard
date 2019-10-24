ea6877ad637706ecc1e9f6bee26b6a32dddafa21
Jagadeeswara Yaramala <yjagspi@gmail.com>
2019-10-24 18:30:46 +0530

    Basic Scatter plot first cut as per new client requirements

--- src/app/components/scatter-plot/scatter-plot.component.ts
+++ src/app/components/scatter-plot/scatter-plot.component.ts
@@ -3,7 +3,7 @@ import { Component, ElementRef, ViewChild, Input, OnInit } from '@angular/core';
 //import { ScatterDataServiceService } from '../../services/scatter-data-service.service';
 
 import { STOCKS } from '../../shared/stock';
-import {ITEMARRIVAL} from '../../shared/item_arrival';
+import { TIMELINES } from '../../shared/item_arrival';
 
 import * as d3 from 'd3-selection';
 import * as d3Scale from 'd3-scale';
@@ -22,21 +22,23 @@ export class ScatterPlotComponent implements OnInit {
 
   // title = 'Line Chart';
 
-  private margin = { top: 20, right: 20, bottom: 30, left: 50 };
+  private margin = { top: 20, right: 50, bottom: 30, left: 50 };
   private width: number;
   private height: number;
   private x: any;
   private y: any;
+  private y1: any;
   private svg: any;
   private line: d3Shape.Line<[number, number]>;
   //private point: d3Shape.Point<[number, number]>;
   xAxisScale: any;
   yAxisScale: any;
+  yAxisRight: any;
 
 
   constructor() {
     this.width = 600 - this.margin.left - this.margin.right;
-    this.height = 500 - this.margin.top - this.margin.bottom;    
+    this.height = 500 - this.margin.top - this.margin.bottom;
   }
 
   ngOnInit() {
@@ -52,54 +54,71 @@ export class ScatterPlotComponent implements OnInit {
       .append('g')
       .attr('transform', 'translate(' + this.margin.left + ',' + this.margin.top + ')');
 
-      this.svg.append("text")
-        .attr("x", (this.width / 2))             
-        .attr("y", this.margin.top / 2)
-        .attr("text-anchor", "middle")  
-        .style("font-size", "16px") 
-        .style("text-decoration", "underline")  
-        .text("Files vs. SLA: Scatterplot");
+    this.svg.append("text")
+      .attr("x", (this.width / 2))
+      .attr("y", this.margin.top / 2)
+      .attr("text-anchor", "middle")
+      .style("font-size", "16px")
+      .style("text-decoration", "underline")
+      .text("Files vs. SLA: Scatterplot");
   }
 
   private initAxis() {
     //this.x = d3Scale.scaleTime().range([0, this.width]);
-    let singleWidth = ITEMARRIVAL.length;
+    let singleWidth = TIMELINES.length;
 
-    this.x = d3Scale.scaleBand().rangeRound([0, this.width]);
-    this.y = d3Scale.scaleLinear().range([this.height, 0]);
-    this.x.domain(ITEMARRIVAL.map((d) => d.item_id));
-    this.y.domain(d3Array.extent(ITEMARRIVAL, (d) => d.arrival_time));
+    this.x = d3Scale.scaleLinear().range([this.width, 0]);
+    //this.x.domain(TIMELINES.map((d) => d.arrival_time));
+    this.x.domain([-30, 30]);
+
+    //this.y = d3Scale.scaleLinear().range([this.height, 0]).ticks(3);
+    //this.y.domain([10, 20]);
+
+    this.y1 = d3Scale.scaleBand().range([this.height, 0]);
+    // this.y1 = d3Scale.scaleBand().range([0, this.height]);
+    this.y1.domain(["Node 1", "Node 2", "Node 3"]);
+
+ 
   }
+
+
 
   private drawAxis() {
 
     this.svg.append('g')
       .attr('class', 'axis axis--x')
       // .attr('transform', 'translate(0,' + this.height + ')')
-      .attr('transform', 'translate(0,' + (this.height / 2) + ')')
-      .call(d3Axis.axisBottom(this.x));      
+      .attr('transform', 'translate(0,' + (this.height) + ')')
+      .call(d3Axis.axisBottom(this.x));
 
-    this.svg.append('g')
-      .attr('class', 'axis axis--y')
-      .call(d3Axis.axisLeft(this.y))
-      .append('text')
-      .attr('class', 'axis-title')
-      .attr('transform', 'rotate(-90)')
-      .attr('y', 6)
-      .attr('dy', '.71em')
-      .style('text-anchor', 'end')
-      .text('Price ($)');
+    // Add the Y1 Axis
+    this.svg.append("g")
+      .attr("class", "axisRed")
+      .attr("transform", "translate( " + this.width + ", 0 )")
+      // .attr('transform', "translate( " + this.width+ ", " + (this.height - (this.height/3)) + ")")
+      .call(d3Axis.axisRight(this.y1));
 
-      // this.svg.append('g')
-      // .attr('class', 'axis axis--y')
-      // .call(d3Axis.axisLeft(this.y))
-      // .append('text')
-      // .attr('class', 'axis-title')
-      // .attr('transform', 'rotate(-270)')
-      // .attr('y', 6)
-      // .attr('dy', '.71em')
-      // .style('text-anchor', 'end')
-      // .text('Price ($)');
+    // this.svg.append('g')
+    //   .attr('class', 'axis axis--y')      
+    //   .append('text')
+    //   .attr('class', 'axis-title')
+    //   .attr('transform', 'translate(270)')
+    //   .call(d3Axis.axisRight(this.y))
+    //   .attr('y', 6)
+    //   .attr('dy', '.71em')
+    //   .style('text-anchor', 'end')
+    //   .text('Price ($)');     
+
+    // this.svg.append('g')
+    // .attr('class', 'axis axis--y')
+    // .call(d3Axis.axisLeft(this.y))
+    // .append('text')
+    // .attr('class', 'axis-title')
+    // .attr('transform', 'rotate(-270)')
+    // .attr('y', 6)
+    // .attr('dy', '.71em')
+    // .style('text-anchor', 'end')
+    // .text('Price ($)');
   }
 
   private drawLine() {
@@ -122,20 +141,39 @@ export class ScatterPlotComponent implements OnInit {
 
     this.svg.append('g')
       .selectAll("dot")
-      .data(ITEMARRIVAL)
+      .data(TIMELINES)
       .enter()
       .append("circle")
       // .attr("cx", function (d) { console.log("d->" + d) ;return this.x(d.date); })
       // .attr("cy", function (d) { return this.y(d.value); })
-      .attr("cx", (d: any) => { console.log("X ->" + this.calcXPlacement(d)); return this.calcXPlacement(d)} )
-      .attr("cy", (d: any) => this.y(d.arrival_time))
+      // .attr("cx", (d: any) => { console.log("X ->" + this.calcXPlacement(d)); return this.calcXPlacement(d) })
+      //.attr("cy", (d: any) => this.y(d.item_id))
+      .attr("cx", (d: any) => { console.log("Cx is: " + this.x(d.arrival_time)); return this.x(d.arrival_time) })
+      .attr("cy", (d: any) => { return this.calcNodeYValues(d) })
       .attr("r", 5)
-      .style("fill", "#69b3a2")
-      // .style("fill", "#FF0000")
+      //.style("fill", "#69b3a2")
+      .style("fill", "#FF0000");
+
+    //Draw a line for the different plots
+    // this.svg.append("path")
+    //   .attr("class", "line")
+    //   .style("stroke-dasharray", ("3, 3"))
+    //   .attr({
+    //     x1: -30, y1: "Node 1",
+    //     x2: 30, y2: "Node 1"
+    //   });
   }
 
-  private calcXPlacement(d:any){
-    let count = ITEMARRIVAL.indexOf(d)+1; 
-    return count*(this.width/ITEMARRIVAL.length);
+  private calcXPlacement(d: any) {
+    let count = TIMELINES.indexOf(d) + 1;
+    return count * (this.width / TIMELINES.length);
+  }
+
+  private calcNodeYValues(d: any) {
+    console.log("Node Number Y is : -> " + this.y1(d.item_id.node_number));
+    console.log("Item Number: -> " + d.item_id.item_number);
+    console.log("Y is -> :" + (this.height - this.y1(d.item_id.node_number) * d.item_id.item_number));
+
+    return  this.y1(d.item_id.node_number) * d.item_id.item_number;
   }
 }
--- src/app/shared/item_arrival.ts
+++ src/app/shared/item_arrival.ts
@@ -1,17 +1,27 @@
 export interface ItemArrival {
-    item_id: string;
+    item_id: Object;
     arrival_time: number;
 }
 
-export const ITEMARRIVAL: ItemArrival[] = [
-    {item_id : "Item 1", arrival_time : 0.75},
-    {item_id : "Item 2", arrival_time : 0.2},
-    {item_id : "Item 3", arrival_time : 1.5},
-    {item_id : "Item 4", arrival_time : -1.5},    
-    {item_id : "Item 5", arrival_time : 0},
-    {item_id : "Item 6", arrival_time : 0}, 
-    {item_id : "Item 7", arrival_time : 1.75}, 
-    {item_id : "Item 8", arrival_time : 0.5}, 
-    {item_id : "Item 9", arrival_time : 0.25}, 
-    {item_id : "Item 10", arrival_time : -0.25}     
+export const TIMELINES: ItemArrival[] = [
+    {item_id : {"node_number" : "Node 1", "item_number" : 0.1}, arrival_time : 0},
+    {item_id : {"node_number" : "Node 1", "item_number" : 0.2}, arrival_time : 0},
+    {item_id : {"node_number" : "Node 1", "item_number" : 0.9}, arrival_time : 1},
+    // {item_id : {"node_id" : "Node 1", "file_id": 2}, arrival_time : 10},
+    // {item_id : {"node_id" : "Node 1", "file_id": 3}, arrival_time : -10},
+    // {item_id : {"node_id" : "Node 2", "file_id": 1}, arrival_time : 0},
+    // {item_id : {"node_id" : "Node 2", "file_id": 2}, arrival_time : 10},
+    // {item_id : {"node_id" : "Node 2", "file_id": 3}, arrival_time : -10},
+    // {item_id : {"node_id" : "Node 3", "file_id": 1}, arrival_time : 0},
+    // {item_id : {"node_id" : "Node 3", "file_id": 2}, arrival_time : 10},
+    // {item_id : {"node_id" : "Node 3", "file_id": 3}, arrival_time : -10},
+    // {item_id : "Item 2", arrival_time : 0.2},
+    // {item_id : "Item 3", arrival_time : 1.5},
+    // {item_id : "Item 4", arrival_time : -1.5},    
+    // {item_id : "Item 5", arrival_time : 0},
+    // {item_id : "Item 6", arrival_time : 0}, 
+    // {item_id : "Item 7", arrival_time : 1.75}, 
+    // {item_id : "Item 8", arrival_time : 0.5}, 
+    // {item_id : "Item 9", arrival_time : 0.25}, 
+    // {item_id : "Item 10", arrival_time : -0.25}     
 ];
